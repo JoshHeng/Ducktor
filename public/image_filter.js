fc032138ee3edd2c0ad6b9894e141c3907ce86d3
SSAFuze <ohwilliamson1@gmail.com>
2022-10-29 16:39:27 +0100

    Remove images with matching hardcoded bad words

---
+++ public/image_filter.js
@@ -0,0 +1,21 @@
+// Obviously only works if image has alt text - which every one should!!!
+
+const bad_words = ["moodle", "cheese"]; // must be lowercase to match with toLowerCase on alt text
+
+function alt_filter(bad_words) {
+    var allImages = document.getElementsByTagName("img");
+    for (var i = 0, len = allImages.length; i < len; ++i) {
+        if (allImages[i].alt !== "") {
+            for (var j = 0, length = bad_words.length; i < length; ++i) {
+                
+                if (allImages[i].alt.toLowerCase().includes(bad_words[j])) {
+                    allImages[i].src = "";
+                    allImages[i].alt = "";
+                    break;
+                }
+            }
+        }
+    }
+}
+
+alt_filter(bad_words);
