ce291349aa297e2e0852e5392f9fc5801352bd04
SSAFuze <ohwilliamson1@gmail.com>
2022-10-29 18:00:29 +0100

    use provided list of bad words

--- public/image_filter.js
+++ public/image_filter.js
@@ -1,12 +1,11 @@
 // Obviously only works if image has alt text - which every one should!!!
 
-const bad_words = ["moodle", "cheese"]; // must be lowercase to match with toLowerCase on alt text
-
 function alt_filter(bad_words) {
     var allImages = document.getElementsByTagName("img");
     for (var i = 0, len = allImages.length; i < len; ++i) {
         if (allImages[i].alt !== "") {
             for (var j = 0, length = bad_words.length; j < length; ++j) {
+                console.log(bad_words[j]);
                 
                 if (allImages[i].alt.toLowerCase().includes(bad_words[j])) {
                     allImages[i].src = "";
@@ -18,4 +17,9 @@ function alt_filter(bad_words) {
     }
 }
 
-alt_filter(bad_words);
+chrome.storage.sync.get(['altTextFilterStrings'], function(result) {
+    console.log('Value currently is ' + result.altTextFilterStrings);
+    let words = result.altTextFilterStrings.split("\n");
+    words = words.filter(element => { return element !== '' }); // avoid any effective widlcards
+    alt_filter(words);
+  });
