a20e66da6ad92907f0c24cc33bf2c29d90f5a72b
Josh Heng <josh@joshheng.co.uk>
2022-10-29 15:44:55 +0100

    Add duck sleep stuff

--- public/background.js
+++ public/background.js
@@ -1,5 +1,9 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-        text: "OFF",
-    });
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    if (msg.action === 'setAwake') {
+        if (msg.value) {
+            chrome.action.setIcon({ path: '/ducks/DuckOn.png' });
+        } else {
+            chrome.action.setIcon({ path: '/ducks/DuckOff.png' });
+        }
+    }
 });
--- src/Modules/App.tsx
+++ src/Modules/App.tsx
@@ -1,16 +1,24 @@
 import React, {useState} from 'react';
 import './App.scss';
 import {FaPaintBrush, FaSun, FaMoon} from 'react-icons/fa';
+import duckImages from '../duckImages.json';
 
 function App() {
     const [ awake, setAwake ] = useState(true);
 
     const toggleAwake = () => {
-        setAwake((_awake) => !_awake);
+        setAwake((_awake) => {
+            chrome?.runtime?.sendMessage({
+                action: 'setAwake',
+                value: !_awake,
+            }).then(r => {});
+
+            return !_awake;
+        });
     }
 
     return (
-        <div className={`app ${awake ? 'awake' : 'asleep'}`}>
+        <div className={`app ${awake ? 'awake' : 'asleep'}`} style={{ backgroundImage: awake ? 'url("/ducks/BackgroundDay.png")' : 'url("/ducks/BackgroundNight.png")'}}>
             <header>
                 <button onClick={toggleAwake}>{ awake ? <FaMoon /> : <FaSun/>}{ awake ? 'Sleep' : 'Wake' }</button>
                 <h1>Ducky</h1>
@@ -18,7 +26,7 @@ function App() {
 
             </header>
             <div className="duck">
-                <img src="/ducks/Duck.png" alt="Duck" />
+                <img src={`/ducks/${awake ? duckImages.none.awake : duckImages.none.asleep}`} alt="Duck" />
             </div>
         </div>
     );
