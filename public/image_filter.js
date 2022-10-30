e330c85116011754ac38bdead99fb2cb797677f5
SSAFuze <ohwilliamson1@gmail.com>
2022-10-30 09:04:12 +0000

    replace images with ducks

--- public/image_filter.js
+++ public/image_filter.js
@@ -2,26 +2,28 @@
 
 function alt_filter(bad_words) {
     let allImages = document.getElementsByTagName("img");
+    var imgURL = chrome.runtime.getURL("ducks/duck.png");
+
     for (let i = 0, len = allImages.length; i < len; ++i) {
         if (allImages[i].alt !== "" || allImages[i].src !== "" || allImages[i].srcset !== "") {
             for (let j = 0, length = bad_words.length; j < length; ++j) {
                 if (allImages[i].alt.toLowerCase().includes(bad_words[j])) {
                     allImages[i].alt = "";
-                    allImages[i].src = "";
+                    allImages[i].src = imgURL;
                     allImages[i].srcset = "";
                     break;
                 }
 
                 else if (allImages[i].src.toLowerCase().includes(bad_words[j])) {
                     allImages[i].alt = "";
-                    allImages[i].src = "";
+                    allImages[i].src = imgURL;
                     allImages[i].srcset = "";
                     break;
                 }
 
                 else if (allImages[i].srcset.toLowerCase().includes(bad_words[j])) {
                     allImages[i].alt = "";
-                    allImages[i].src = "";
+                    allImages[i].src = imgURL;
                     allImages[i].srcset = "";
                     break;
                 }
